9599d0eeabd45768f549f39a79bbd7b9c89bdf4f
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-11 18:13:04 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color
-  attr_accessor :height, :weight, :handed, :complexion
-  attr_accessor :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :birthday, :hair_color, :eye_color,
+  attr_accessor :height, :weight, :handed, :complexion,
+  attr_accessor :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width,
   
   def initialize(*attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
