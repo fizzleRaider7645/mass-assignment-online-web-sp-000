9194247fc47c54f36fbdfa006aa20556c26dd7df
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-11 18:11:32 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,7 @@
 class Person
   attr_accessor: :name, :birthday
-  
+  attr_accessor:
+  attr_accessor:
   
   def initialize(*attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
