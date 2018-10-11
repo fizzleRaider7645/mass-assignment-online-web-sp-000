6ac48fcd7401344ddebf6109f2b9acd59da9eb3a
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-11 18:11:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   attr_accessor: :name, :birthday, :hair_color, :eye_color,
-  attr_accessor: :hair_color, :eye_color,
+  attr_accessor: 
   attr_accessor:
   
   def initialize(*attributes)
